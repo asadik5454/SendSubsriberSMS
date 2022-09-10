061ed39256a917ce3b0a204bf3e46b41542c7ba5
asadik5454 <59363079+asadik5454@users.noreply.github.com>
2022-09-10 23:28:47 +0500

    Init.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,74 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import org.apache.commons.httpclient.util.DateUtil;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String connectionUrl = "jdbc:postgresql://tjk-zetalif01:5432/zetalifDB";
+        String userName = "zetalif";
+        String password = "JnmnF54D3Op";
+        System.out.println(LocalDateTime.now().plusDays(1).format(DateTimeFormatter.ISO_DATE));
+        System.out.println(LocalDateTime.now().plusDays(2).format(Da));
+
+
+
+
+        String getNumber = "";
+        String getComName = "";
+        String getComNameTj = "";
+        int getAmount = 0;
+        String getNextDate = "";
+        int getId = 0;
+        try (Connection connection = DriverManager.getConnection(connectionUrl, userName, password)) {
+            Methods.send_sms("992911003819", "Старт Смс");
+
+            Statement statementSelect = connection.createStatement();
+            Statement statementUpdate = connection.createStatement();
+            try {
+                int i = 0;
+                ResultSet resultSet = statementSelect.executeQuery("select id, number , commercial_name , amount, next_apply_date from payment_per_day where sms_is_send = 'false' and commercial_name is not null;");
+                while (resultSet.next()) {
+                    getId = resultSet.getInt("id");
+                    getNumber = resultSet.getString("number");
+                    getComName = resultSet.getString("commercial_name");
+                    getComNameTj = resultSet.getString("commercial_name");
+                    getAmount = resultSet.getInt("amount");
+                    getNextDate = resultSet.getString("next_apply_date");
+                    getComName = "\"" + getComName + "\"";
+                    getComNameTj = "\"" + getComNameTj + "\"";
+                    getId = resultSet.getInt("id");
+                    System.out.println(" Это количествооо :" + i++);
+                    String sms = "Уважаемый абонент! Срок действия пакета " + getComName + " истекает "  + getNextDate
+                            + ". Списание абонентской платы в размере " + getAmount + "  сомони произойдет в ночь с "
+                            + getNextDate + " на " + LocalDateTime.parse(getNextDate).plusDays(1).toString()
+                            + "  Просим Вас иметь на балансе достаточную сумму на момент списания абонентской платы\n"
+                            + getNextDate + " Муштарии мухтарам! Мухлати амали  бастаи  " + getComNameTj + " " + getNextDate + ". тамом мешавад. Муштарипули ба андозаи " + getAmount + " сомони шаби аз "
+                            + getNextDate + " ба " + LocalDateTime.parse(getNextDate).plusDays(1).toString()
+                            +", гирифта мешавад. Хохишмандем, ки хангоми гирифтани муштарипули, дар суратхисоб маблаги лозима дошта бошед.";
+                    String SQL = "update payment_per_day set text_sms = '" + sms + "' , sms_is_send = 'true' where sms_is_send = 'false' and id = " + getId + ";";
+                    Methods.send_sms("992911003819", sms);
+                 //   Methods.send_sms(getNumber, sms);
+                    System.out.println(SQL);
+                    System.out.println(getNumber + " " + getComName + " " + getAmount + " " + getNextDate);
+                    boolean resultSet1 = statementUpdate.execute(SQL);
+                                    }
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+
+
+
+    }
+} catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/example/Methods.java
+++ src/main/java/org/example/Methods.java
@@ -1,2 +1,56 @@
-package org.example;public class Methods {
+package org.example;
+
+import org.json.simple.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Methods {
+
+    public static void send_sms(String subs_key, String text_sms) throws Exception {
+        JSONObject jo = new JSONObject();
+        try {
+            System.out.println("Start sms");
+            jo.put("msisdn", subs_key);
+            jo.put("source_addr","ZET-MOBILE");//"ZET-MOBILE"
+            jo.put("text", text_sms);
+
+            String ip_address = "http://172.28.193.139:8070/data";
+            byte[] data = jo.toString().getBytes( StandardCharsets.UTF_8 );
+            int dataLength = jo.toString().length();
+
+            URL obj = new URL(ip_address);
+
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setDoOutput( true );
+            con.setInstanceFollowRedirects( false );
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Mozilla5/0");
+            con.setRequestProperty("Accept-Charset", "");
+            con.setRequestProperty("Content-Length", Integer.toString(dataLength));
+            try( DataOutputStream wr = new DataOutputStream( con.getOutputStream())) {
+                wr.write( data );
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            StringBuffer response = new StringBuffer();
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            in.close();
+            System.out.println("response = " + response);
+
+        } catch (Exception e) {
+        }
+    }
+
+
+
+
+
 }
